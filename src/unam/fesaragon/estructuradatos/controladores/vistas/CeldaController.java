a2ed6420f6189564c3569e23ed406ba078d5b411
Carlos Saul <carlossaul.pazm@gmail.com>
2024-09-26 23:40:14 -0600

    Creando metodo para llenar la cuadricula de celdas y modificar su tamano

--- src/unam/fesaragon/estructuradatos/controladores/vistas/CeldaController.java
+++ src/unam/fesaragon/estructuradatos/controladores/vistas/CeldaController.java
@@ -1,10 +1,15 @@
 package unam.fesaragon.estructuradatos.controladores.vistas;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
 import unam.fesaragon.estructuradatos.modelos.laberinto.Coordenada;
 
 public class CeldaController {
     private Coordenada coordenada;
+    public static final String urlFXMLDeCelda = "src/unam/fesaragon/estructuradatos/vistas/celda.fxml";
+
+    @FXML
+    private Pane panelCelda;
 
     public CeldaController() {
     }
@@ -14,6 +19,9 @@ public class CeldaController {
         System.out.println("Click");
     }
 
+    public Pane getPanelCelda() {
+        return panelCelda;
+    }
 
     public Coordenada getCoordenada() {
         return coordenada;
--- src/unam/fesaragon/estructuradatos/modelos/javafx/Cuadricula.java
+++ src/unam/fesaragon/estructuradatos/modelos/javafx/Cuadricula.java
@@ -1,27 +1,52 @@
 package unam.fesaragon.estructuradatos.modelos.javafx;
 
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import unam.fesaragon.estructuradatos.controladores.vistas.CeldaController;
+import unam.fesaragon.estructuradatos.controladores.vistas.CuadriculaController;
+
+import java.io.IOException;
 
 public class Cuadricula {
-    private static String urlFXML = "unam/fesaragon/estructuradatos/vistas/cuadricula.fxml";
+    public static final String urlFXMLDeCuadricula = "src/unam/fesaragon/estructuradatos/vistas/cuadricula.fxml";
     //Componentes de la cuadricula
     private AnchorPane anchorPaneContainerCuadricula;
-    //Componentes de la celda
-    private GridPane gridPane;
-    private Pane pane;
+    private CuadriculaController cuadriculaController;
 
+    //Atributos de la cuadricula
+    private int filas;
+    private int columnas;
 
-    private void cargarFXML(){
+    public Cuadricula(int filas, int columnas) {
+        this.filas = filas;
+        this.columnas = columnas;
+        cargarComponentes();
+    }
+
+    private void cargarComponentes() {
+        FXMLLoader loaderCuadricula = new FXMLLoader(getClass().getResource(Cuadricula.urlFXMLDeCuadricula));
         try {
-            FXMLLoader leerFXML = FXMLLoader.load(getClass().getResource(urlFXML));
-             anchorPaneContainerCuadricula= leerFXML.load();
+            //Cargando la cuadricula
+            anchorPaneContainerCuadricula = loaderCuadricula.load();
+            cuadriculaController = loaderCuadricula.getController();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
-        }catch (Exception e ){
-            System.out.println("Error al cargar la escena: \n" + e);
+        llenarCuadriculaDeCeldas();
+
+    }
+
+    private void llenarCuadriculaDeCeldas() {
+        //Cargando la celda
+        FXMLLoader loaderCelda = new FXMLLoader(getClass().getResource(CeldaController.urlFXMLDeCelda));
+        try {
+            GridPane gridPane = loaderCelda.load();
+            CeldaController celdaController = loaderCelda.getController();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
